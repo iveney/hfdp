86808c480703a073f2b597c6412daa56b05fb6ee
Zigang Xiao <iveney@gmail.com>
2013-09-28 13:28:07 -0500

    Weather using java.util.Observer

---
+++ weather_java/StatisticsDisplay.java
@@ -0,0 +1,38 @@
+import java.util.Observer;
+import java.util.Observable;
+
+public class StatisticsDisplay implements Observer, DisplayElement {
+	Observable observable;
+	private float maxTemp = 0.0f;
+	private float minTemp = 200;
+	private float tempSum= 0.0f;
+	private int numReadings;
+	
+	public StatisticsDisplay(Observable observable) {
+		this.observable = observable;
+		observable.addObserver(this);
+	}
+
+	public void update(Observable obs, Object arg) {
+		if (obs instanceof WeatherData) {
+			WeatherData weatherData = (WeatherData) obs;
+			float temp = weatherData.getTemperature();
+			tempSum += temp;
+			numReadings++;
+
+			if (temp > maxTemp) {
+				maxTemp = temp;
+			}
+
+			if (temp < minTemp) {
+				minTemp = temp;
+			}
+			display();
+		}
+	}
+
+	public void display() {
+		System.out.println("Avg/Max/Min temperature = " + (tempSum / numReadings)
+				+ "/" + maxTemp + "/" + minTemp);
+	}
+}
